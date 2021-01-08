BerkeleyLibrary/tind

Initial project setup

rakelib/gem.rake
@@ -1,0 +1,56 @@
+require 'rubygems/gem_runner'
+require 'ucblit/logging/module_info'
+
+module UCBLIT
+  module Logging
+    class << self
+      def project_root
+        @project_root ||= File.expand_path('..', __dir__)
+      end
+
+      def artifacts_dir
+        return project_root unless ENV['CI']
+
+        @artifacts_dir ||= File.join(project_root, 'artifacts')
+      end
+
+      def gemspec_file
+        @gemspec_file ||= begin
+          gemspec_files = Dir.glob(File.expand_path('*.gemspec', project_root))
+          raise ArgumentError, "Too many .gemspecs: #{gemspec_files.join(', ')}" if gemspec_files.size > 1
+          raise ArgumentError, 'No .gemspec file found' if gemspec_files.empty?
+
+          gemspec_files[0]
+        end
+      end
+
+      def gemspec_basename
+        File.basename(gemspec_file)
+      end
+
+      def output_file
+        @output_file ||= begin
+          gem_name = File.basename(gemspec_file, '.*')
+          version = UCBLIT::Logging::ModuleInfo::VERSION
+          basename = "#{gem_name}-#{version}.gem"
+          File.join(artifacts_dir, basename)
+        end
+      end
+
+      def output_file_relative
+        return File.basename(output_file) unless ENV['CI']
+
+        @output_file_relative ||= begin
+          artifacts_dir_relative = File.basename(artifacts_dir)
+          File.join(artifacts_dir_relative, File.basename(output_file))
+        end
+      end
+    end
+  end
+end
+
+desc "Build #{UCBLIT::Logging.gemspec_basename} as #{UCBLIT::Logging.output_file_relative}"
+task :gem do
+  args = ['build', UCBLIT::Logging.gemspec_file, "--output=#{UCBLIT::Logging.output_file}"]
+  Gem::GemRunner.new.run(args)
+end

BerkeleyLibrary/marc-spec

rakelib/gem.rake
@@ -1,0 +1,56 @@
+require 'rubygems/gem_runner'
+require 'marc/spec/module_info'
+
+module MARC
+  module Spec
+    class << self
+      def project_root
+        @project_root ||= File.expand_path('..', __dir__)
+      end
+
+      def artifacts_dir
+        return project_root unless ENV['CI']
+
+        @artifacts_dir ||= File.join(project_root, 'artifacts')
+      end
+
+      def gemspec_file
+        @gemspec_file ||= begin
+          gemspec_files = Dir.glob(File.expand_path('*.gemspec', project_root))
+          raise ArgumentError, "Too many .gemspecs: #{gemspec_files.join(', ')}" if gemspec_files.size > 1
+          raise ArgumentError, 'No .gemspec file found' if gemspec_files.empty?
+
+          gemspec_files[0]
+        end
+      end
+
+      def gemspec_basename
+        File.basename(gemspec_file)
+      end
+
+      def output_file
+        @output_file ||= begin
+          gem_name = File.basename(gemspec_file, '.*')
+          version = MARC::Spec::ModuleInfo::VERSION
+          basename = "#{gem_name}-#{version}.gem"
+          File.join(artifacts_dir, basename)
+        end
+      end
+
+      def output_file_relative
+        return File.basename(output_file) unless ENV['CI']
+
+        @output_file_relative ||= begin
+          artifacts_dir_relative = File.basename(artifacts_dir)
+          File.join(artifacts_dir_relative, File.basename(output_file))
+        end
+      end
+    end
+  end
+end
+
+desc "Build #{MARC::Spec.gemspec_basename} as #{MARC::Spec.output_file_relative}"
+task :gem do
+  args = ['build', MARC::Spec.gemspec_file, "--output=#{MARC::Spec.output_file}"]
+  Gem::GemRunner.new.run(args)
+end