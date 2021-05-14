01d4cb787e37817bf0cf8d974f14624969b4fbbc
David Moles <dmoles@dmoles.net>
2021-05-14 14:03:31 -0700

    Add some scratch cases

--- spec/scratch_spec.rb
+++ spec/scratch_spec.rb
@@ -40,6 +40,38 @@ module MARC
           puts result.inspect
         end
       end
+
+      describe :subfield_spec do
+        it 'parses' do
+          data = '245$a'
+          result = try_parse(data, :subfield_spec)
+          puts result.inspect
+        end
+
+        it 'parses as a sub_term' do
+          data = '245$a'
+          result = try_parse(data, :sub_term)
+          puts result.inspect
+        end
+
+        it 'parses as a sub_term_set' do
+          data = '245$a'
+          result = try_parse(data, :sub_term_set)
+          puts result.inspect
+        end
+
+        it 'parses as a sub_term_set with an operator' do
+          data = '245$a=246$b'
+          result = try_parse(data, :sub_term_set)
+          puts result.inspect
+        end
+
+        it 'parses as a sub_spec body' do
+          data = '{245$a}'
+          result = try_parse(data, :sub_spec)
+          puts result.inspect
+        end
+      end
     end
   end
 end
